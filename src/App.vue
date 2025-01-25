86272a348fef0eba7a490ccec758d7b3bab618dd
Maik Fröbe <maik.froebe@uni-jena.de>
2025-01-26 00:51:32 +0100

    mf

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app>
+    <home-menu/>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="ts">
+import HomeMenu from './components/HomeMenu.vue'
+export default {
+  name: "app",
+  components: {HomeMenu}
+}
 </script>
---
+++ src/components/HomeMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar style="background-color: #323232; color: white;">
+    <v-container style="max-width: 1110px;">
+      <v-row>
+        <v-col>
+          <router-link to="/">
+            <img src="/favicon.png" alt="Maik Fr&ouml;be" style="height: 2.667em">
+          </router-link>
+        </v-col>
+
+        <v-col class="text-right">
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props">
+                <v-icon>mdi-menu</v-icon>
+              </v-btn>
+            </template>
+            
+          </v-menu>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-app-bar>
+</template>
